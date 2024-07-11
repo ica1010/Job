20e65bc139859a3757fe39c313673d09217812d7
ica1010 <ghislinametonou@gmail.com>
2024-07-11 21:59:37 +0000

    static file

--- core/core/settings.py
+++ core/core/settings.py
@@ -105,6 +105,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'core/static/')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 if not DEBUG:
